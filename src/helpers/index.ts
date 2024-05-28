d71c4382c298c555e96ac0ee81c5fd0451917ec2
dementorpro <dementopro47@gmail.com>
2024-05-28 18:56:58 +0400

    added the functions to query images

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -5,35 +5,41 @@ const apiKey = process.env.flickrApiKey as string;
 
 const loadImages = async (tags: string[]) => {
   try {
-    axios({
-      method: 'get',
-      url: 'https://api.flickr.com/services/rest',
-      params: {
-        method: 'flickr.photos.search',
-        api_key: apiKey,
-        tags: "cat",
-        page: 1,
-        per_page: 500,
-        format: 'json',
-        extras: 'url_n, date_upload, tags',
-        nojsoncallback: 1
-      }
-    })
-    .then(async res => {
-      Promise.all(
-        res.data.photos.photo.map(async (item: any) => {
-          const dbConnection = await DBInit.connect();
-          return await dbConnection.query(
-            `INSERT INTO photos
-            ("publishedDate", "imageUrl", "tags")
-            VALUES (to_timestamp($1), $2, $3)
-            RETURNING id`,
-            [item.dateupload, item.url_n, item.tags],
-          )
-        })
-      )
-      console.log('Data saved')
-    })
+    const dbConnection = await DBInit.connect();
+    const result = await dbConnection.query(
+      `SELECT * FROM photos`
+    )
+    if (result.rows.length === 0) {
+      axios({
+        method: 'get',
+        url: 'https://api.flickr.com/services/rest',
+        params: {
+          method: 'flickr.photos.search',
+          api_key: apiKey,
+          tags: "cat",
+          page: 1,
+          per_page: 500,
+          format: 'json',
+          extras: 'url_n, date_upload, tags',
+          nojsoncallback: 1
+        }
+      })
+      .then(async res => {
+        Promise.all(
+          res.data.photos.photo.map(async (item: any) => {
+            const dbConnection = await DBInit.connect();
+            return await dbConnection.query(
+              `INSERT INTO photos
+              ("publishedDate", "imageUrl", "tags")
+              VALUES (to_timestamp($1), $2, $3)
+              RETURNING id`,
+              [item.dateupload, item.url_n, item.tags],
+            )
+          })
+        )
+        console.log('Data saved')
+      })
+    }
   } catch (error) {
     console.log("Error: ", error);
   }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,43 @@
 import express from 'express';
 import { AppDataSource } from './data-source';
 import loadImages from './helpers';
-
+import * as DBInit from './database/init';
+const cors = require('cors');
 
 AppDataSource.initialize().then(() => {
    const app = express();
-   app.use(express.json())
-   app.get('/', (req, res) => {
-   return res.json('Established connection!');
+   app.use(express.json());
+   app.use(cors());
+   app.get('/images/:page/:tag', async (req, res) => {
+    const { page, tag } = req.params;
+    const dbConnection = await DBInit.connect();
+    const photos = tag !== "all" ? 
+    await dbConnection.query(
+      `SELECT * FROM photos WHERE "tags" LIKE '%${tag}%' ORDER BY "publishedDate" DESC LIMIT 10 OFFSET ${(Number(page) - 1) * 10}`
+    )
+    : await dbConnection.query(
+      `SELECT * FROM photos ORDER BY "publishedDate" DESC LIMIT 10 OFFSET ${(Number(page) - 1) * 10}`
+    ) 
+    const topTags = await dbConnection.query(
+      `
+      select tag, count(*) as frequency from (
+      select unnest(regexp_split_to_array(tags, ' ')) as tag from photos
+      ) as tag_table
+      group by tag
+      order by frequency desc
+      limit 10
+      `
+    );
+
+    return res.json({
+      photos: photos.rows,
+      topTags: topTags.rows,
+    });
  })
 
   loadImages(["cat"]);
 
-  return app.listen(process.env.PORT);
+  return app.listen(process.env.PORT, () => {
+    console.log("APP is running on port: ", process.env.PORT);
+  });
 })
